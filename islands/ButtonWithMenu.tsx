bbda97a0c153d503568217fa7208aeb0ef5bd0e9
Cyprien de Fontenay <cbdefontenay@gmail.com>
2023-05-09 15:26:19 +0200

    changement dans about page

--- islands/ButtonWithMenu.tsx
+++ islands/ButtonWithMenu.tsx
@@ -16,33 +16,40 @@ const ButtonWithMenu = () => {
         <div className="absolute left-0 w-full bg-red-800 text-gray-300 text-center border border-gray-200 rounded-md shadow-lg transition-opacity ease-in-out duration-300">
           <a
             href="#"
-            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-100"
+            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-300 hover:text-red-700"
           >
-            Item 1
+            JORVIK Story
           </a>
           <a
             href="#"
-            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-100"
+            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-300 hover:text-red-700"
           >
-            Item 2
+            JORVIK Artefact Gallery
           </a>
           <a
             href="#"
-            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-100"
+            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-300 hover:text-red-700"
           >
-            Item 3
+            York Archeological Trust
           </a>
           <a
             href="#"
-            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-100"
+            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-300 hover:text-red-700"
           >
-            Item 4
+            Support Us
           </a>
           <a
             href="#"
-            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-100"
+            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-300 hover:text-red-700"
           >
-            Item 5
+            Touring Exhibitions
+          </a>
+
+          <a
+            href="#"
+            className="block w-full px-4 py-2 text-sm text-gray-300 hover:bg-gray-300 hover:text-red-700"
+          >
+            Environmental Sustainability
           </a>
         </div>
       )}
--- routes/about.tsx
+++ routes/about.tsx
@@ -4,7 +4,7 @@ import ButtonWithMenu from "../islands/ButtonWithMenu.tsx";
 const About = () => {
   return (
     <>
-      <div>
+      <div className="h-screen">
         <VideoAbout />
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-2">
           <img
@@ -26,6 +26,60 @@ const About = () => {
           </h2>
 
           <ButtonWithMenu />
+        </div>
+      </div>
+
+      <div className="bg-gray-200 h-full pt-10 relative w-full lg:h-full lg:py-12 lg:px-40 2xl:pt-28">
+        <div class="flex flex-col md:flex-row">
+          <h2 class="text-red-800 text-3xl text-center md:w-1/4 md:mt-28 lg:text-4xl md:ml-32">
+            JORVIK Viking Centre
+          </h2>
+          <div class=" h-screen px-4 pb-8 text-justify mt-8 md:max-w-3xl">
+            <div class="mb-4 lg:text-2xl">
+              <p class="mb-4">
+                At the world famous JORVIK Viking Centre you are standing on the
+                site of one of the most famous and astounding discoveries of
+                modern archaeology. Between the years 1976-81 archaeologists
+                from York Archaeological Trust, an independent educational
+                charity, revealed the houses, workshops and backyards of the
+                Viking Age city of Jorvik as it stood nearly 1,000 years ago.
+              </p>
+              <p class="mb-4">
+                These incredible discoveries enabled us to build JORVIK Viking
+                Centre on the very site where the excavations had taken place,
+                creating a groundbreaking visitor experience where you take a
+                journey through the reconstruction of Viking Age streets and
+                experience life as it would have been in 10th century York.
+              </p>
+            </div>
+
+            <div class="relative flex flex-col mt-10 mb-10 md:flex-row md:mt-44">
+              <div class="flex justify-center md:mt-4 lg:mt-4">
+                <img
+                  src="/achterbahn.webp"
+                  alt="montagnes russes"
+                  class="rounded-lg max-w-full"
+                />
+              </div>
+              <div class="absolute bg-red-800 p-4 w-52 h-44 rounded-md md:w-6/12 md:h-4/6 top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:-top-0 lg:left-0 md:self-center md:mt-10 md:ml-8">
+                <h1 class="text-yellow-600 text-2xl text-center font-bold mb-2 md:pt-8 md:px-8 md:text-3xl">
+                  Discover Viking York
+                </h1>
+                <p class="text-gray-300 text-sm text-center md:pb-8 md:px-8 md:text-2xl">
+                  Explore what was found at the astonishing Coppergate dig
+                </p>
+              </div>
+            </div>
+
+            <div className="">
+              <p className="mt-12 text-gray-500 md:text-xl lg:text-2xl">
+                &nbsp;With an updated ride experience and state-of-the art
+                galleries showcasing our unique collection of Viking artefacts
+                visit the re-imagined JORVIK Viking Centre and discover York’s
+                fascinating Viking legacy.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
