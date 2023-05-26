fa8009eb126ad7e24b9ba372553cdb99e5e46447
Cyprien de Fontenay <cbdefontenay@gmail.com>
2023-05-26 22:07:38 +0200

    ...

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,16 +6,16 @@ import FourthPage from "../components/layout/FourthPage.tsx";
 import SecondPage from "../components/layout/SecondPage.tsx";
 import ThirdPage from "../components/layout/ThirdPage.tsx";
 import Transition from "../components/layout/Transition.tsx";
-import { useEffect } from "preact/hooks";
-import ReactGA from "npm:react-ga";
+// import { useEffect } from "preact/hooks";
+// import ReactGA from "npm:react-ga";
 
-const TRACKING_ID = "UA-256329277-3";
-ReactGA.initialize(TRACKING_ID);
+// const TRACKING_ID = "UA-256329277-3";
+// ReactGA.initialize(TRACKING_ID);
 
 export default function Home() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname);
-  }, []);
+  // useEffect(() => {
+  //   ReactGA.pageview(window.location.pathname);
+  // }, []);
   return (
     <>
       <Head>
